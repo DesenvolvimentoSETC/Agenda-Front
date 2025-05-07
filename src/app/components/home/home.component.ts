6aa854ee7b0745d604d0d8d51d389fc68281ddf2
desenvolvimento.setc <desenvolvimento.setc@gmail.com>
2025-05-07 11:38:49 -0100

    First commit

---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,59 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { MatDatepicker, MatDatepickerModule } from '@angular/material/datepicker';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatCalendar } from '@angular/material/datepicker';
+import { MAT_DATE_LOCALE } from '@angular/material/core';
+import { Y } from '@angular/cdk/keycodes';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [RouterModule,
+    CommonModule,
+    MatDatepickerModule,
+    MatCardModule,
+    MatIconModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatCalendar
+  ],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+  dataSelecionada = new Date(2025, 4, 5);  // Corrigido para maio de 2025 (0-indexado)
+  dataInterna: Date | null = null;
+  descricao = 'Teste';
+  hora = '10h00';
+  Local = 'Teatro Atheneu';
+  mostrarAviso = false;
+
+  
+
+  // Adicionando eventos para datas específicas
+  eventos: Date[] = [
+    new Date(2025, 4, 10),  // 10 de maio de 2025
+    new Date(2025, 4, 15),  // 15 de maio de 2025
+    new Date(2025, 4, 20)   // 20 de maio de 2025
+  ];
+
+  verificarEvento(data: Date) {
+    this.dataSelecionada = data;
+
+    // Verifica se a data selecionada está na lista de eventos
+    const temEvento = this.eventos.some(evento =>
+      evento.toDateString() === data.toDateString()
+    );
+
+    // Exibe ou não a mensagem de aviso
+    this.mostrarAviso = !temEvento;
+    
+    setTimeout(() => {
+      this.dataInterna = null; // permite novo clique no calendário
+    });
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,17 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import localePt from '@angular/common/locales/pt';
+import { LOCALE_ID } from '@angular/core';
+import { MAT_DATE_LOCALE } from '@angular/material/core';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+
+import { registerLocaleData } from '@angular/common';
+registerLocaleData(localePt);
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    { provide: LOCALE_ID, useValue: 'pt-BR' },
+    { provide: MAT_DATE_LOCALE, useValue: 'pt-BR' }
+  ]
+});
