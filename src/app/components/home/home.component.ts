bc924ca3f07228d4758f21dab92c909943d6c0e7
desenvolvimento.setc <desenvolvimento.setc@gmail.com>
2025-06-18 12:05:36 -0300

    Configurar BreadCrumps

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -8,6 +8,8 @@ import { MatNativeDateModule } from '@angular/material/core';
 
 import { AgendaService } from '../../services/agenda.service';
 import { AgendaDTO } from '../../agendas/DTO/AgendaDTO';
+import { AuthService } from '../../auth/auth.service';
+
 
 @Component({
   selector: 'app-home',
@@ -34,7 +36,7 @@ export class HomeComponent implements OnInit {
   
   agendas: AgendaDTO[] = []; 
 
-  constructor(private router: Router, private agendaService: AgendaService) {}
+  constructor(private router: Router, private agendaService: AgendaService, private authService: AuthService) {}
 
   ngOnInit(): void {
     this.getAllAgendas();
@@ -44,19 +46,14 @@ export class HomeComponent implements OnInit {
     console.log('Buscando todos os eventos da agenda...');
     this.agendaService.getAllAgendas().subscribe({
       next: (data) => {
-        // CORREÇÃO AQUI: Ajustar a data para garantir que seja exibida corretamente
         this.agendas = data.map(agenda => {
-          // Cria um objeto Date no fuso horário local a partir da string "YYYY-MM-DD"
-          // Isso evita o problema de fuso horário que causa o "dia a menos".
           const [year, month, day] = agenda.data.split('-').map(Number);
-          // O mês em JavaScript é baseado em 0 (janeiro é 0, fevereiro é 1, etc.)
           const localDate = new Date(year, month - 1, day); 
 
-          return { ...agenda, data: localDate.toISOString().split('T')[0] }; // Mantém a string no formato YYYY-MM-DD para AgendaDTO, mas garante a consistência para exibição
+          return { ...agenda, data: localDate.toISOString().split('T')[0] }; 
         });
         console.log('Eventos da agenda recebidos e ajustados:', this.agendas);
 
-        // Chame verificarEvento para a data atual assim que os eventos forem carregados
         this.verificarEvento(this.dataSelecionada);
       },
       error: (err) => {
@@ -66,22 +63,27 @@ export class HomeComponent implements OnInit {
   }
 
   irParaCadastro() {
-    this.router.navigate(['/admin']);
+    console.log('Tentando acessar Área do Administrador');
+    this.authService.logout(); 
   }
 
-  /**
-   * Verifica se há um evento na data selecionada no calendário
-   * e atualiza as informações de descrição, hora e local na UI.
-   * @param data A data selecionada pelo usuário (um objeto Date).
-   */
-  verificarEvento(data: Date) {
-    this.dataSelecionada = data; // Atualiza a data selecionada na propriedade do componente
+   navigateToHomeInfo(): void {
+    window.open('https://transparencia.se.gov.br/comum/informacoes.html', '_blank');
+  }
 
-    // Converte a data selecionada para uma string YYYY-MM-DD para comparação
-    // (Ajuste para garantir que a data no objeto 'agenda' seja também uma string YYYY-MM-DD)
+  navigateToOrganizationalStructure(): void {
+    window.open('https://transparencia.se.gov.br/EstruturaOrganizacional/EstruturaOrganizacional.xhtml', '_blank');
+  }
+
+  redirectToExternalAgenda(): void {
+    window.open('https://transparencia.se.gov.br/EstruturaOrganizacional/AgendaGovernador.xhtml?faces-redirect=true', '_blank');
+  }
+
+  verificarEvento(data: Date) {
+    this.dataSelecionada = data; 
+
     const selectedDateString = data.toISOString().split('T')[0];
 
-    // Encontra o evento correspondente na lista de agendas
     const eventoEncontrado = this.agendas.find(agenda => {
       // Compara a string de data do evento com a string de data selecionada
       return agenda.data === selectedDateString;
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -3,13 +3,14 @@ import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common'; 
 import { AuthService } from '../../auth/auth.service'; 
 import { Router } from '@angular/router'; 
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-login', 
   templateUrl: './login.component.html', 
   styleUrls: ['./login.component.css'], 
   standalone: true, 
-  imports: [FormsModule, CommonModule] 
+  imports: [FormsModule, CommonModule, MatIconModule,] 
 })
 export class LoginComponent {
   username: string = '';
@@ -17,6 +18,21 @@ export class LoginComponent {
   loginError: string | null = null; 
 
   constructor(private authService: AuthService, private router: Router) { } 
+
+   navigateToHomeInfo(): void {
+    window.open('https://transparencia.se.gov.br/comum/informacoes.html', '_blank');
+  }
+
+  navigateToOrganizationalStructure(): void {
+    window.open('https://transparencia.se.gov.br/EstruturaOrganizacional/EstruturaOrganizacional.xhtml', '_blank');
+  }
+
+  redirectToExternalAgenda(): void {
+    window.open('https://transparencia.se.gov.br/EstruturaOrganizacional/AgendaGovernador.xhtml?faces-redirect=true', '_blank');
+  }
+  navigateToAgenda(): void {
+    window.open('http://localhost:4200');
+  }
 
   onLogin(): void {
     this.loginError = null; 
@@ -28,7 +44,7 @@ export class LoginComponent {
           console.log('Login bem-sucedido no componente!');
           this.router.navigate(['/admin']);
         } else {
-          this.loginError = 'Falha no login. Verifique o console para mais detalhes.';
+          this.loginError = 'Falha no login. Verifique suas credenciais ou a rede.';
         }
       },
       error: (err) => {
