7239735dc79182a9758b2db752527df38ffebb48
desenvolvimento.setc <desenvolvimento.setc@gmail.com>
2025-07-10 11:57:46 -0300

    First commit

--- src/app/agendas/DTO/AgendaDTO.ts
+++ src/app/agendas/DTO/AgendaDTO.ts
@@ -1,9 +1,7 @@
 export interface AgendaDTO {
-  id?: number; // Opcional, pois pode não estar presente ao criar um novo evento (ID é gerado pelo DB)
+  id?: number; 
   data: string; // Representa a data (ex: "YYYY-MM-DD")
   descricao: string; // Representa a descrição do evento
   hora: string; // Representa a hora (ex: "HH:mm:ss")
   local: string; // Representa o local do evento
-  // Adicione aqui outros campos que sua API retorna para a entidade Agenda,
-  // como 'titulo', 'responsavel', etc., se eles existirem na sua entidade Spring Boot.
 }
