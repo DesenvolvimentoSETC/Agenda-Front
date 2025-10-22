9c155207005330a0353c846f56906a1523e22af0
andersongabriel <gl5270357@gmail.com>
2025-10-22 14:28:14 -0300

    fix: Corrige HTTPS

--- src/environments/environment.test.ts
+++ src/environments/environment.test.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   // TEMPORÁRIO: Usando proxy CORS para teste (NÃO usar em produção)
-  apiUrl: 'https://cors-anywhere.herokuapp.com/http://172.22.21.17:9090',
+  apiUrl: 'https://cors-anywhere.herokuapp.com/https://172.22.21.17:9090',
   frontendUrl: 'https://desenvolvimentosetc.github.io/Agenda-Front'
 };
