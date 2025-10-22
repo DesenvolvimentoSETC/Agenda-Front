36b28dec84cebed39ca97bd3c5347bf25969b60c
andersongabriel <gl5270357@gmail.com>
2025-10-22 14:22:48 -0300

    fix: Corrige HTTPS

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://172.22.21.17:9090',
+  apiUrl: 'https://172.22.21.17:9090',
   frontendUrl: 'https://desenvolvimentosetc.github.io/Agenda-Front/'
 };
